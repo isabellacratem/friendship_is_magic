d97fc5664102ed488203d525bf12616073afa817
isabellacratem <156118701+isabellacratem@users.noreply.github.com>
2025-02-20 12:00:47 -0500

    idk how to use visual studio

--- excursion_1.cpp
+++ excursion_1.cpp
@@ -1,2 +1,35 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
+
+
+/*Input: a file named textlist.txt w the format
+ "branch label (V1,R1)| source node label (0,1,2) | destination label (0,1,2) | value (5,10,20)"
+ Output: a file named output.txt w the format potentials, voltages, currents with one space between them
+ "e1 e2 ...en v1 v2... vn i1 i2...in" 
+ These values are floating-point numbers w 3 decimal places. */
+
+ int main() {
+
+    // Open the text file named "netlist.txt"
+    ifstream f("netlist.txt");
+
+    // Check if the file is successfully opened
+    if (!f.is_open()) {
+        cerr << "Error opening the file!";
+        return 1;
+    }
+
+    // String variable to store the read data
+    string s;
+
+    // Read each line of the file and print it to the
+    // standard output stream till the whole file is
+  	// completely read
+    while (getline(f, s))
+        cout << s << endl;
+
+    // Close the file
+    f.close();
+    return 0;
+} 
