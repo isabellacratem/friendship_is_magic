8fe5b8e4673f2cc7d44ca60ea9b0f4fa7632a977
isabellacratem <156118701+isabellacratem@users.noreply.github.com>
2025-02-24 12:49:49 -0500

    updated main file

--- excursion_1.cpp
+++ excursion_1.cpp
@@ -9,25 +9,24 @@ using namespace std;
  "e1 e2 ...en v1 v2... vn i1 i2...in" 
  These values are floating-point numbers w 3 decimal places. */
 
-void readFile (string file){
+string readFile (string file){
      // Open the text file named "netlist.txt"
-     ifstream f("netlist.txt");
+     ifstream f(file);
      
-
-
      //this is rly basic code that needs to be changed- my idea is read each line as a string and add it to an array
     // Read each line of the file and print it to the
     // standard output stream till the whole file is
   	// completely read
-    string s;
-     while (getline(f, s))
-     cout << s << endl;
-
+    string s, line;
+    while (getline(f, line)) {
+        s += line + "\n";
+    } 
     // Close the file
     f.close();
-
+  return s;
 }
 
  int main() {
-
+  string s=readFile("netlist.txt");
+  cout <<s;
 } 
